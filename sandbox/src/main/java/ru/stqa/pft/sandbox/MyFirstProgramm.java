7e84e676fb632f63126270a8a9540ae0d2751141
KatyaPryadkina <101329639+KatyaPryadkina@users.noreply.github.com>
2022-03-11 16:13:47 +0700

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -1,8 +1,8 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgramm {
-	
-	public static void main(String[] args) {
-System.out.println("Hello, world !!!!");
-	}
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world !!!!");
+  }
 }
