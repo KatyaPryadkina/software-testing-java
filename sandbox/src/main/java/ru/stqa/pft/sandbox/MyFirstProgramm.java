4509f18797d587651129395693405d17ca50a840
KatyaPryadkina <101329639+KatyaPryadkina@users.noreply.github.com>
2022-03-16 15:59:00 +0700

    Задание № 2, использование функции, классов, объектов и методов (методы закоментир)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -6,18 +6,40 @@ public class MyFirstProgramm {
     hello("world");
     hello("user");
     hello("Katya!");
+   //квадрат
     Square s = new Square(5);//объявление переменной при указании его в кач-ве атрибута в функции
    // s.l = 5; убрали из-за того что параметр заполнен выше
     System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    //прямоугольник
     Renctagle k = new Renctagle(4,6);
-    //k.a = 4;
-   // k.b = 6;
-    System.out.println("Площадь прямоугольника со сторонами "+  k.a + " и " + k.b + " = " + k.area());
+    /*k.a = 4;
+    k.b = 6;*/
+   System.out.println("Площадь прямоугольника со сторонами "+  k.a + " и " + k.b + " = " + k.area());
+
+    //расстояние между точками
+
+    Point p1 = new Point();
+    Point p2 = new Point();
+    p1.x1 = 2.0;
+    p2.x2 = 4.1;
+    p1.y1 = 3.1;
+    p2.y2 = 2.2;
+    System.out.println("Расстояние между точками "+ "(" + p1.x1 + "," + p1.y1 + ")" + ":" + "(" + p2.x2 + "," + p2.y2 + ")" + " = " +  distance(p1,p2));
+
+     /*Point p1 = new Point().p1(2.0,3.1);
+     Point p2 = new Point().p2(4.1,2.2);
+
+     System.out.println("Расстояние между точками"+ p1 + ":" + p2 + " = " +  distance()) ;*/
   }
+
+  public static  double distance(Point p1, Point p2){
+         return Math.sqrt((p2.x2 - p1.x1) * (p2.x2 - p1.x1) + (p2.y2 - p1.y1)*(p2.y2 - p1.y1));
+  }
+
+
   public static void hello (String somebody) {
 
     System.out.println("Hello, " + somebody + "!!!!");
   }
-
-
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,4 +1,33 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
+  public double x1;
+  public double y1;
+  public double x2;
+  public double y2;
+  public double d;
+
+//конструктор
+  /*public Point p1 (double x1, double y1) {
+    this.x1 = x1;
+    this.y1 = y1;
+    return null;
+  }
+  public Point p2 (double x2, double y2) {
+    this.x2 = x2;
+    this.y2 = y2;
+    return null;
+  }
+  //функция
+   public   double distance(){
+     d =   Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1)*(this.y2 - this.y1));
+     return d ;
+  }*/
+
 }
+
+
+
+
+
+
