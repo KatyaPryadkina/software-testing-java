aad5ee9e6cc54766d5c5875be9019a0ba46c6375
KatyaPryadkina <101329639+KatyaPryadkina@users.noreply.github.com>
2022-03-18 23:14:38 +0700

    Задание № 2 - исправления

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -6,40 +6,38 @@ public class MyFirstProgramm {
     hello("world");
     hello("user");
     hello("Katya!");
-   //квадрат
+    //квадрат
     Square s = new Square(5);//объявление переменной при указании его в кач-ве атрибута в функции
-   // s.l = 5; убрали из-за того что параметр заполнен выше
+    // s.l = 5; убрали из-за того что параметр заполнен выше
     System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
     //прямоугольник
-    Renctagle k = new Renctagle(4,6);
+    Renctagle k = new Renctagle(4, 6);
     /*k.a = 4;
     k.b = 6;*/
-   System.out.println("Площадь прямоугольника со сторонами "+  k.a + " и " + k.b + " = " + k.area());
+    System.out.println("Площадь прямоугольника со сторонами " + k.a + " и " + k.b + " = " + k.area());
 
     //расстояние между точками
+    Point p1 = new Point(2, 3.1);
+    Point p2 = new Point(4.1, 2.2);
 
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x1 = 2.0;
-    p2.x2 = 4.1;
-    p1.y1 = 3.1;
-    p2.y2 = 2.2;
-    System.out.println("Расстояние между точками "+ "(" + p1.x1 + "," + p1.y1 + ")" + ":" + "(" + p2.x2 + "," + p2.y2 + ")" + " = " +  distance(p1,p2));
+    System.out.println("Расстояние между точками " + "(" + p1.x + "," + p1.y + ")" + ":" + "(" + p2.x + "," + p2.y + ")" + " = " + p1.distance(p1, p2));
 
-     /*Point p1 = new Point().p1(2.0,3.1);
-     Point p2 = new Point().p2(4.1,2.2);
+//ниже описано обращение к методу в рамках одного класса
+    /*Point p1 = new Point(2.0 , 3.1);
+     Point p2 = new Point(4.1,2.2);
 
-     System.out.println("Расстояние между точками"+ p1 + ":" + p2 + " = " +  distance()) ;*/
-  }
+    System.out.println("Расстояние между точками "+ "(" + p1.x + "," + p1.y + ")" + ":" + "(" + p2.x + "," + p2.y + ")" + " = " +  distance(p1,p2));
+
+       }
 
   public static  double distance(Point p1, Point p2){
-         return Math.sqrt((p2.x2 - p1.x1) * (p2.x2 - p1.x1) + (p2.y2 - p1.y1)*(p2.y2 - p1.y1));
+         return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+  }*/
   }
 
-
-  public static void hello (String somebody) {
-
+  public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!!!!");
   }
 }
+
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,28 +1,23 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  public double x1;
-  public double y1;
-  public double x2;
-  public double y2;
-  public double d;
+  public double x;
+  public double y;
+
 
 //конструктор
-  /*public Point p1 (double x1, double y1) {
-    this.x1 = x1;
-    this.y1 = y1;
-    return null;
+  public  Point (double x, double y) {
+    this.x = x;
+    this.y = y;
+
+
   }
-  public Point p2 (double x2, double y2) {
-    this.x2 = x2;
-    this.y2 = y2;
-    return null;
-  }
+
   //функция
-   public   double distance(){
-     d =   Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1)*(this.y2 - this.y1));
-     return d ;
-  }*/
+   public  static double distance(Point p1, Point p2){
+     return   Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+
+  }
 
 }
 
