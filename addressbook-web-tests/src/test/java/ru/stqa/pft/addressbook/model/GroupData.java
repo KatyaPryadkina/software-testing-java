f8572c1fffd3c011b71e5063bc27725c01c69649
KatyaPryadkina <101329639+KatyaPryadkina@users.noreply.github.com>
2022-04-02 21:05:27 +0700

    4.6 Добавлено сравнение списка групп целиком

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,6 +1,27 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public record GroupData(String name, String header, String footer) {
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            '}';
+  }
 }
 //так описывается более ранняя форма записи:  создания класса типа record
 /*public class GroupData {
