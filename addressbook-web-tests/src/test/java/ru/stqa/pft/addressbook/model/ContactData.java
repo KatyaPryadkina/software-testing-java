e660eb68b951325a62ed1a992d6a1de6d6317ca8
KatyaPryadkina <101329639+KatyaPryadkina@users.noreply.github.com>
2023-01-17 17:25:48 +0700

    Лекция 7.3. Условия отбора данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,37 +3,88 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
-
+@Entity
+@Table(name="addressbook")
 public class ContactData{
   @XStreamOmitField
+  @Id
+  @Column(name="id")
   private  Integer id = Integer.MAX_VALUE;
   @Expose
+  @Column(name="firstname")
   private  String firstname;
-  @Expose private  String lastname;
+  @Expose
+  @Column(name="lastname")
+  private  String lastname;
+  @Column(name="nickname")
   private  String nickname;
-  @Expose private  String address;
-  @Expose private  String email1;
+  @Expose
+  @Column(name="address")
+  @Type(type="text")
+  private  String address;
+  @Expose
+  @Column(name="email")
+  @Type(type="text")
+  private  String email1;
+
+  @Column(name="email2")
+  @Type(type="text")
   private  String email2;
+
+  @Column(name="email3")
+  @Type(type="text")
   private  String email3;
-  @Expose  private  String mobilNumber;
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "id=" + id +
+            ", firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            '}';
+  }
+
+  @Expose
+  @Column(name="mobile")
+  @Type(type="text")
+  private  String mobilNumber;
+
+  @Column(name="home")
+  @Type(type="text")
   private  String homeNumber;
+
+  @Column(name="work")
+  @Type(type="text")
   private  String workNumber;
+
+  @Transient
   private  String group;
+
+  @Transient
   private String allPhones;
+
+  @Transient
   private String allEmail;
-  private File photo;
+
+  @Column(name="photo")
+  @Type(type="text")
+  private String photo;
+
+
 
   public File getPhoto() {
-    return photo;
+    return new File (photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
@@ -214,15 +265,6 @@ public class ContactData{
 
   public String getGroup() {
     return group;
-  }
-
-  @Override
-  public String toString() {
-    return "ContactData{" +
-            "id=" + id +
-            ", firstname='" + firstname + '\'' +
-            ", lastname='" + lastname + '\'' +
-            '}';
   }
 
   @Override
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -7,6 +7,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
@@ -33,12 +34,24 @@ public class HbConnectionTest {
     }
 
     @Test
-    public void testHbConnection (){
+    public void testHbConnectionGroup (){
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        List result = session.createQuery( "from GroupData" ).list();
+        List result = session.createQuery( "from GroupData" ).list(); // запрос к объекту, вместо запроса sql
         for (GroupData group : (List<GroupData>) result) {
             System.out.println(group);
+        }
+        session.getTransaction().commit();
+        session.close();
+
+    }
+    @Test
+    public void testHbConnectionContact (){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List result = session.createQuery( "from ContactData where deprecated = '0000-00-00'" ).list(); // запрос к объекту, вместо запроса sql, кроме удаленных с датой deprecated
+        for (ContactData contact : (List<ContactData>) result) {
+            System.out.println(contact);
         }
         session.getTransaction().commit();
         session.close();
