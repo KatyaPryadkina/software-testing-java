e4a5e79b15d6ee202b4e4c3be7a33570f06f2b8e
KatyaPryadkina <101329639+KatyaPryadkina@users.noreply.github.com>
2023-01-17 23:39:13 +0700

    Лекция 7.5. Отключаемые проверки UI

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -39,7 +39,12 @@ public class GroupModificationTests extends TestBase {
 
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
 
+    verifyGroupListInUI();
+    
+
   }
+
+
 
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -4,6 +4,13 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -23,5 +30,17 @@ public class TestBase {
 
   public ApplicationManager getApp() {
     return app;
+  }
+  public void verifyGroupListInUI() {
+    if (Boolean.getBoolean("verifyUI")){                        // VM options : -DverifyUI true - включаем проверку в "Edit Configurations"
+      Groups dbGroups = app.db().groups();
+      Groups uiGroups = app.group().all();
+      assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map((g) -> new GroupData().withId(g.getid()).withName(g.getName())) // упростим полученные с бд данные и оставим для сравнения только те данные, которые есть на ui
+              .collect(Collectors.toSet())));// соберем эти данные в коллекцию
+
+    }
+
+
   }
 }
